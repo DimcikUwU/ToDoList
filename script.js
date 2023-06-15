f1a76fff7c8ff02f0850326bbc0065cc4f7ac9d5
DimcikUwU <stepangroger@gmail.com>
2023-06-15 10:23:36 +0200

    skoro final

--- script.js
+++ script.js
@@ -9,7 +9,28 @@ function addTask(){
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         let span= document.createElement("span")
-        span.innerHTML = "\u00d7"
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     inputBox.value = "";
+    saveTasks();
 }
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveTasks();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveTasks();
+    }
+},false)
+
+function saveTasks(){
+    localStorage.setItem("tasks", listContainer.innerHTML);
+}
+
+function showTasks(){
+    listContainer.innerHTML = localStorage.getItem("tasks");
+}
+showTasks();
